69454019b404f6093ed142f0cba8280b9d58e61f
Matt Welsh <mdw@mdw.la>
2016-08-10 22:32:25 -0700

    Adding signin

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 // Team Sidney
 
 var count = 0;
+
+$('#login').css('color', 'blue');
+$('#login').css('text-decoration', 'underline');
+$('#login').text('Sign in');
+$('#login').click(login);
+$('#userinfo').text('No user yet');
 $('#button').text('Click me');
 $('#current').text('No value yet');
+
+var provider = new firebase.auth.GoogleAuthProvider();
 
 var countRef = firebase.database().ref('stats/count');
 countRef.on('value', function(snapshot) {
@@ -16,3 +24,32 @@ $('#button').click(function() {
     count: count
   });
 });
+
+function login() {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // Do nothing on success.
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    $('#userinfo').text('Sorry, could not log you in: ' + errorMessage);
+    $('#login').show();
+  });
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    $('#login').hide();
+    $('#userinfo').css('color', 'black');
+    $('#userinfo').css('text-decoration', 'none');
+    $('#userinfo').text('Hi, ' + user.displayName + '!');
+  } else {
+    $('#login').show();
+    $('#userinfo').text('No user yet');
+  }
+});
+
