9821ba20b3a15ce833b8ec180e06d9d2025044b7
Matt Welsh <mdw@mdw.la>
2016-08-10 22:08:21 -0700

    Read/write counter

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 // Team Sidney
 
-$('button').text = 'This is a button';
-$('current').text = 'This is current';
+var count = 0;
+$('#button').text('Click me');
+$('#current').text('No value yet');
+
+var countRef = firebase.database().ref('stats/count');
+countRef.on('value', function(snapshot) {
+  count = snapshot.val();
+  $('#current').text('Button clicked ' + count + ' times');
+});
+
+$('#button').click(function() {
+  count++;
+  firebase.database().ref('stats').set({
+    count: count
+  });
+});
