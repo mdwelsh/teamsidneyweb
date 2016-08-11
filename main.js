41807f486a4664b2715facc40b42bde2214f71fa
Matt Welsh <mdw@mdw.la>
2016-08-10 22:43:30 -0700

    Add logout

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ $('#login').css('color', 'blue');
 $('#login').css('text-decoration', 'underline');
 $('#login').text('Sign in');
 $('#login').click(login);
+
+$('#logout').hide();
+$('#logout').css('color', 'red');
+$('#logout').css('text-decoration', 'underline');
+$('#logout').text('Sign out');
+$('#logout').click(logout);
+
 $('#userinfo').text('No user yet');
 $('#button').text('Click me');
 $('#current').text('No value yet');
@@ -27,28 +34,32 @@ $('#button').click(function() {
 
 function login() {
   firebase.auth().signInWithPopup(provider).then(function(result) {
-    // Do nothing on success.
+    $('#login').hide();
+    $('#logout').show();
   }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    $('#userinfo').text('Sorry, could not log you in: ' + errorMessage);
+    $('#userinfo').text('Sorry, could not log you in: ' + error.message);
     $('#login').show();
+    $('#logout').hide();
+  });
+}
+
+function logout() {
+  firebase.auth().signOut().then(function() {
+    $('#login').show();
+    $('#logout').hide();
+  }, function(error) {
+    $('#userinfo').text('Sorry, could not log you out: ' + error.message);
+    $('#login').hide();
+    $('#logout').show();
   });
 }
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
-    $('#login').hide();
     $('#userinfo').css('color', 'black');
     $('#userinfo').css('text-decoration', 'none');
     $('#userinfo').text('Hi, ' + user.displayName + '!');
   } else {
-    $('#login').show();
     $('#userinfo').text('No user yet');
   }
 });
