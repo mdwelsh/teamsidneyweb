38d0f7f536cc4275455f81219bff8e8a6806a724
Matt Welsh <mdw@mdw.la>
2019-02-04 15:37:24 -0800

    Working on etching

--- escher/main.js
+++ escher/main.js
@@ -9,16 +9,13 @@ window.onload = function() {
   var image = new Image();
   image.src = "EtchASketch.jpg";
   $(image).load(function () {
-    console.log('Image width ' + image.width);
-    console.log('Image height ' + image.height);
-    console.log('Canvas width ' + c.get(0).width);
-    console.log('Canvas height ' + c.get(0).height);
     ctx.drawImage(image, 0, 0, c.get(0).width, c.get(0).height);
+    etch();
+  });
 
+  resizeCanvas();
+  $(window).on("resize", function(){
     resizeCanvas();
-    $(window).on("resize", function(){
-      resizeCanvas();
-    });
   });
 
 };
@@ -30,6 +27,29 @@ function resizeCanvas(){
   var ow = $("#etchCanvas").outerWidth;
   // This is the background image height and width.
   // Yeah, I'm being lazy by hardcoding these.
-  var oh = int(ow * (1081.0 / 1326.0));
+  var oh = (ow * (1081.0 / 1326.0));
   $("#etchCanvas").outerHeight(oh);
+}
+
+function etch(points) {
+  // Offsets to screen of Etch-a-Sketch (experimentally determined).
+  var sx0 = 210;
+  var sy0 = 210;
+  var sw = 900;
+  var sh = 620;
+
+  var c = $("#etchCanvas");
+  var ctx = c.get(0).getContext("2d");
+
+  // Debugging - Draw blue border.
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 2;
+  ctx.strokeRect(sx0, sy0, sw, sh);
+
+  points.forEach(function(elem) {
+    var x = elem.x;
+    var y = elem.y;
+  });
+
+  console.log('Etching done');
 }
