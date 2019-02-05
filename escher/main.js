70a9a68c48674fc2f077ec07e9c2acf470e3fbb0
Matt Welsh <mdw@mdw.la>
2019-02-04 19:20:00 -0800

    Starting on gcode upload

--- escher/main.js
+++ escher/main.js
@@ -17,8 +17,104 @@ window.onload = function() {
   $(window).on("resize", function(){
     resizeCanvas();
   });
-
 };
+
+function setup() {
+  // Upload Gcode actions.
+  $('#fileUploadButton').click(function (e) {
+    uploadGcodeStart();
+    $("#uploadGcode").get()[0].showModal();
+  });
+  $('#uploadGcodeConfirm').click(function (e) {
+    $("#uploadGcode").get()[0].close();
+    gcodeUploadDone();
+  });
+  $('#uploadGcodeCancel').click(function (e) {
+    $("#uploadGcode").get()[0].close();
+  });
+  $('#uploadGcodeClose').click(function (e) {
+    $("#uploadGcode").get()[0].close();
+  });
+  $('#uploadGcodeFile').change(function() {
+    var file = $('#uploadGcodeFile')[0].files[0];
+    uploadGcode(file);
+  });
+}
+
+var uploadedGcode = null;
+var uploadedGcodeUrl = null;
+
+function uploadGcodeStart() {
+  $('#uploadGcodeSelectedFile').empty();
+  $('#uploadGcodeError').empty();
+  $('#uploadGcodeLink').empty();
+  $('#uploadGcodeSpinner').hide();
+}
+
+function uploadGcode(file) {
+  uploadedGcode = file;
+  $('#uploadGcodeConfirm').prop('disabled', true);
+  $('#uploadGcodeSelectedFile').text('File: ' + file.name);
+  $('#uploadGcodeError').empty();
+  $('#uploadGcodeLink').empty();
+
+  var reader = new FileReader();
+  reader.onloadend = function() {
+    uploadGcodeFinished(reader.result);
+  }
+  reader.readAsArrayBuffer(file);
+}
+
+// XXX XXX MDW Update below for Gcode
+function uploadFirmwareFinished(data) {
+  uploadedFirmwareVersion = getFirmwareVersion(data);
+  if (uploadedFirmwareVersion == null) {
+    $('#uploadFirmwareError')
+      .text('File missing magic version string. Is this a Blinky binary?');
+    uploadedFirmware = null;
+    return;
+  }
+
+  // Valid binary - make it possible to add.
+  console.log('Got firmware version: ' + uploadedFirmwareVersion);
+  $('#uploadFirmwareVersion').text('Version string: ' + uploadedFirmwareVersion);
+  $('#uploadFirmwareConfirm').prop('disabled', false);
+
+  // Start the upload.
+  $('#uploadFirmwareSpinner').show();
+  var fname = uploadedFile.name + ' ' + uploadedFirmwareVersion;
+  console.log('Starting upload of ' + fname);
+  var uploadRef = storageRef.child(fname);
+  uploadRef.put(uploadedFile).then(function(snapshot) {
+    // Upload done.
+    $('#uploadFirmwareSpinner').hide();
+    console.log('Upload complete');
+    // Add link.
+    uploadRef.getDownloadURL().then(function(url) {
+      uploadedFirmwareUrl = url;
+      $('#uploadFirmwareLink').html('Uploaded: <a href="'+url+'">'+fname+'</a>');
+    });
+  });
+}
+
+function firmwareUploadDone() {
+  var dbRef = firebase.database().ref('firmware/' + uploadedFirmwareVersion);
+  var metadata = {
+    dateUploaded: firebase.database.ServerValue.TIMESTAMP,
+    version: uploadedFirmwareVersion,
+    filename: uploadedFile.name + ' ' + uploadedFirmwareVersion,
+    url: uploadedFirmwareUrl,
+  };
+  dbRef.set(metadata).then(function() {
+    addLogEntry('added firmware '+uploadedFirmwareVersion);
+  })
+  .catch(function(error) {
+    $('#uploadFirmwareError').text(error.message);
+  });
+}
+// XXX XXX MDW Update above for Gcode
+
+
 
 // Scale canvas outerWidth/outerHeight to match dimensions
 // of background image.
